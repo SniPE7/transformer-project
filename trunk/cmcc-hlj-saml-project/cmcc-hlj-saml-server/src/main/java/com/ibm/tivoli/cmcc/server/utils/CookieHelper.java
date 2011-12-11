ee23572a08f17ff5e69a7322fec5ecf6cef61755
machozhao@gmail.com <machozhao@gmail.com@b9b41c21-3e89-f5b9-be48-c26063835e23>
2011-12-11 11:32:05 +0000

    解决Cookie自动被Tomcat添加""的问题.

--- trunk/cmcc-hlj-saml-project/cmcc-hlj-saml-server/src/main/java/com/ibm/tivoli/cmcc/server/utils/CookieHelper.java
+++ trunk/cmcc-hlj-saml-project/cmcc-hlj-saml-server/src/main/java/com/ibm/tivoli/cmcc/server/utils/CookieHelper.java
@@ -7,12 +7,18 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+
 public class CookieHelper {
 
   private static final String CM_TOKENID = "cmtokenid";
 
   public static void saveArtifactIdIntoCookies(HttpServletResponse response, String artifactID, String artifactDomain, String cookieDomain) {
-    saveToCookies(response, cookieDomain, CM_TOKENID, artifactID + "@" + artifactDomain);
+    if (!StringUtils.isEmpty(System.getProperty("catalina.home"))) {
+      saveToCookies4Tomcat(response, cookieDomain, CM_TOKENID, artifactID + "@" + artifactDomain);
+    } else {
+      saveToCookies(response, cookieDomain, CM_TOKENID, artifactID + "@" + artifactDomain);
+    }
   }
 
   /**
@@ -27,6 +33,18 @@ public class CookieHelper {
     cookie.setDomain(cookieDomain);
     cookie.setPath("/");
     response.addCookie(cookie);
+  }
+
+  /**
+   * 由于移动规范中定义的Cookie值中包含@特殊字符, 此字符不被Cookie允许, 所以造成Tomcat会为其增加一对", 为了克服此问题, 需绕过Tomcat的处理机制, 直接输出Cookie.
+   * WebSphere行为目前没有测试.
+   * 
+   * @param response
+   * @param key
+   * @param value
+   */
+  public static void saveToCookies4Tomcat(HttpServletResponse response, String cookieDomain, String key, String value) {
+    response.addHeader("Set-Cookie", String.format("%s=%s; domain=%s; path=/", key, value, cookieDomain));
   }
 
   /**
