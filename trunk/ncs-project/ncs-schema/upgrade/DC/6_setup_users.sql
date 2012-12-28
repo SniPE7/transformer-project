d2798d681366da75bf769725713e367b6e4d938f
machozhao@gmail.com <machozhao@gmail.com@b9b41c21-3e89-f5b9-be48-c26063835e23>
2012-12-28 06:30:41 +0000

    WAS 7 运行正常.

--- trunk/ncs-project/ncs-schema/upgrade/DC/6_setup_users.sql
+++ trunk/ncs-project/ncs-schema/upgrade/DC/6_setup_users.sql
@@ -7,4 +7,11 @@ insert into t_user_role_map(usid, role_id) values(0, 0);
 -- 将ncc用户赋权限为operator角色
 insert into t_user_role_map(usid, role_id) values(1, 1);
 
+-- 创建用户
+--insert into t_user(USID, UNAME, PASSWORD) values(100, 'ahncc', 'ahncc');
+-- 将ncc用户赋权限为operator角色
+--insert into t_role(role_id, role_name) values(100, 'ahoperator');
+--insert into t_user_role_map(usid, role_id) values(100, 100);
+--insert into T_ROLE_MANAGED_NODE(role_id, server_id) values(100, 100);
+
 commit;
