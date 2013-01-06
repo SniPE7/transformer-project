53575546b455d0f2a9ab9423854497145ee9e96d
machozhao <machozhao@gmail.com>
2013-01-06 14:47:29 +0000

    确保升级程序不被多用户并发执行.

--- ncs-project/ncs-web-module/src/main/java/com/ibm/ncs/web/policyapply/UpgradePoliciesController.java
+++ ncs-project/ncs-web-module/src/main/java/com/ibm/ncs/web/policyapply/UpgradePoliciesController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.ibm.ncs.model.dao.PolicyPublishInfo;
 import com.ibm.ncs.model.dao.TPolicyPublishInfoDao;
@@ -64,36 +65,47 @@ public class UpgradePoliciesController implements Controller {
 	 * .http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    String operation = request.getParameter("operation");
-    
+		String operation = request.getParameter("operation");
+
 		Map<String, Object> model = new HashMap<String, Object>();
 		message = "";
+		boolean doing = request.getSession().getServletContext().getAttribute("DOING_POLICY_UPGRATION") != null
+		    && ((Boolean) request.getSession().getServletContext().getAttribute("DOING_POLICY_UPGRATION")).booleanValue();
 		if (operation != null && operation.equals("upgrade")) {
-    	try {
-	      StringWriter writer = new StringWriter();
+			try {
+
+				StringWriter writer = new StringWriter();
 				writer.write("<pre>\n");
-				this.policyPublishInfoDao.upgrade(writer);
-				writer.write("升级成功!\n");
+				if (!doing) {
+					request.getSession().getServletContext().setAttribute("DOING_POLICY_UPGRATION", true);
+					this.policyPublishInfoDao.upgrade(writer);
+					//Thread.sleep(20000);
+					request.getSession().getServletContext().removeAttribute("DOING_POLICY_UPGRATION");
+					writer.write("升级成功!\n");
+				} else {
+					writer.write("<font color='red'>其它用户已经运行升级程序, 现在无法运行, 稍后请查询升级的结果!</font>");
+					doing = true;
+				}
 				writer.write("</pre>\n");
 				model.put("operationMessage", writer.toString());
-      } catch (Exception e) {
-  			message = e.getMessage();
-  			Log4jInit.ncsLog.error(this.getClass().getName() + " Error occured:\n" + e.getMessage(), e);
-      }
-    } else if (operation != null && operation.equals("migrate")) {
-    	try {
-	      StringWriter writer = new StringWriter();
+			} catch (Exception e) {
+				message = e.getMessage();
+				Log4jInit.ncsLog.error(this.getClass().getName() + " Error occured:\n" + e.getMessage(), e);
+			}
+		} else if (operation != null && operation.equals("migrate")) {
+			try {
+				StringWriter writer = new StringWriter();
 				writer.write("<pre>\n");
 				this.policyPublishInfoDao.migrate(writer);
 				writer.write("迁移成功!\n");
 				writer.write("</pre>\n");
 				model.put("operationMessage", writer.toString());
-      } catch (Exception e) {
-  			message = e.getMessage();
-  			Log4jInit.ncsLog.error(this.getClass().getName() + " Error occured:\n" + e.getMessage(), e);
-      }
-    }
-    
+			} catch (Exception e) {
+				message = e.getMessage();
+				Log4jInit.ncsLog.error(this.getClass().getName() + " Error occured:\n" + e.getMessage(), e);
+			}
+		}
+
 		boolean needUpgrade = false;
 		boolean needMigrate = false;
 		try {
@@ -110,15 +122,25 @@ public class UpgradePoliciesController implements Controller {
 			} else if (releasedPolicyPublishInfo == null) {
 				message = "未发布策略模板, 无需升级本地策略定义!";
 			} else if (appliedPolicyPublishInfo == null) {
-				message = "已发布策略模板, 本地从未应用策略模板，需迁移本地策略定义!";
-				needMigrate = true;
+				if (doing) {
+					message = "已发布策略模板, 本地从未应用策略模板，需迁移本地策略定义 其他用户已经运行升级程序, 请稍候!";
+					needMigrate = true;
+				} else {
+					message = "已发布策略模板, 本地从未应用策略模板，需迁移本地策略定义!";
+					needMigrate = true;
+				}
 			} else if (releasedPolicyPublishInfo != null && appliedPolicyPublishInfo != null) {
 				if (releasedPolicyPublishInfo.getPpiid() == appliedPolicyPublishInfo.getPpiid()) {
 					// Not need upgrade
 					message = "版本一致, 无需升级本地策略定义!";
 				} else {
-					message = "版本不一致, 需升级本地策略定义!";
-					needUpgrade = true;
+					if (doing) {
+						message = "版本不一致, 其他用户已经运行升级程序, 请稍候!";
+						needUpgrade = true;
+					} else {
+						message = "版本不一致, 需升级本地策略定义!";
+						needUpgrade = true;
+					}
 				}
 			}
 		} catch (Exception e) {
