e63708d8faf2c00178d297d19cdeb6a34bb90107
machozhao@gmail.com <machozhao@gmail.com@b9b41c21-3e89-f5b9-be48-c26063835e23>
2013-08-09 06:37:06 +0000

    支持使用MatchCode登录, 目前的机制是： 1、界面提交的是Card UID 2.查询目录如果使用Card UID能够找到用户，其它密码验证通过的情况下，用户认证成功 3.若不成功，通过MatchCodeService传入CardUID换算出MatchCode 4.使用MatchCode查询目录（SGMBadgeCode），找到用户，且其它密码等验证通过的情况下，用户认证成功 5.如果使用MatchCode认证成功，则需要更新用户的属性，保存CardUID到SGMBadgeID中
    实现步骤5涉及的保存功能。
    
    另外，实现对于非TIM帐号保存手机和修改密码时，直接存放到TAM LDAP目录。

---
+++ trunk/security-eai-webmodule/src/test/java/com/sinopec/siam/am/idp/authn/service/BaseTestCase.java
@@ -0,0 +1,35 @@
+package com.sinopec.siam.am.idp.authn.service;
+
+import junit.framework.TestCase;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public abstract class BaseTestCase extends TestCase {
+
+	protected ApplicationContext context = null;
+
+	public BaseTestCase() {
+		super();
+	}
+
+	public BaseTestCase(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		String[] locations = {"/eai/conf/spring-beans-config.xml", 
+				"/eai/conf/spring-beans-loginmodule-jaas.xml", 
+				"/eai/conf/spring-beans-loginmodule-tamldap.xml", 
+				"/eai/conf/spring-beans-matchcode.xml",
+				"/eai/conf/spring-beans-sms.xml",
+				"/eai/conf/spring-beans-util.xml"};
+	  context = new ClassPathXmlApplicationContext(locations);
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+}
